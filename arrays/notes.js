e560b3a3ab6725d9951dc54049d7fe0e27b23515
António Barbosa <86804002+nhilus@users.noreply.github.com>
2022-05-21 15:38:11 +0100

    arrays intro + challenge

---
+++ arrays/notes.js
@@ -0,0 +1,7 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+console.log(notes)
+
+console.log(notes[0])
+
+console.log(notes[notes.length - 1])
---
+++ arrays/todo.js
@@ -0,0 +1,11 @@
+//challenge:
+//create array with 5 todos
+//you have x todos
+//print the first and second to last items -> Todo:walk the dog
+
+
+const todos = ['clean', 'cook','walk the dog', 'study', 'eat' ]
+
+console.log(`You have ${todos.length} todos`)
+console.log(`Todo: ${todos[todos.length - 1]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
