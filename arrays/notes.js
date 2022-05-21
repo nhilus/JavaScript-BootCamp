8909a1df1b1f0c085970b02eaa7745ffca37dea6
António Barbosa <86804002+nhilus@users.noreply.github.com>
2022-05-21 16:04:30 +0100

    array manipulation

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,7 +1,22 @@
 const notes = ['Note 1', 'Note 2', 'Note 3']
 
+//end of array
+
+//console.log(notes.pop())
+//notes.push('My new note')
+
+//begining of array
+
+//console.log(notes.shift())
+//notes.unshift('My first note')
+
+//middle of array
+
+//notes.splice(1, 1)
+//notes.splice(1, 1, 'This is my new second item')
+
+notes[2] = 'This is the new note 3'
+
+console.log(notes.length)
 console.log(notes)
 
-console.log(notes[0])
-
-console.log(notes[notes.length - 1])
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,11 +1,27 @@
-//challenge:
+//challenge 1:
 //create array with 5 todos
 //you have x todos
 //print the first and second to last items -> Todo:walk the dog
 
-
 const todos = ['clean', 'cook','walk the dog', 'study', 'eat' ]
 
+//console.log(`You have ${todos.length} todos`)
+//console.log(`Todo: ${todos[0]}`)
+//console.log(`Todo: ${todos[todos.length - 2]}`)
+
+//challenge 2:
+
+//delete the third item
+//add a new item to the end
+//remove the first item from the list
+
+
 console.log(`You have ${todos.length} todos`)
-console.log(`Todo: ${todos[todos.length - 1]}`)
-console.log(`Todo: ${todos[todos.length - 2]}`)
+console.log(todos)
+
+todos.splice(2, 1)
+todos.push('this is a new last task')
+todos.shift()
+
+console.log(`You have ${todos.length} todos`)
+console.log(todos)
