6f737f14525db20f5e05b283a82f7761e9fceca3
António Barbosa <86804002+nhilus@users.noreply.github.com>
2022-05-21 16:39:48 +0100

    looping(forEach)

--- arrays/notes.js
+++ arrays/notes.js
@@ -20,3 +20,9 @@ notes[2] = 'This is the new note 3'
 console.log(notes.length)
 console.log(notes)
 
+//looping of array
+notes.forEach(function(item, index){
+    console.log(index)
+    console.log(item)
+})
+
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,20 +1,20 @@
+const todos = ['clean', 'cook','walk the dog', 'study', 'eat' ]
+
 //challenge 1:
 //create array with 5 todos
 //you have x todos
 //print the first and second to last items -> Todo:walk the dog
 
-const todos = ['clean', 'cook','walk the dog', 'study', 'eat' ]
 
 //console.log(`You have ${todos.length} todos`)
 //console.log(`Todo: ${todos[0]}`)
 //console.log(`Todo: ${todos[todos.length - 2]}`)
 
-//challenge 2:
 
+//challenge 2:
 //delete the third item
 //add a new item to the end
 //remove the first item from the list
-
 
 console.log(`You have ${todos.length} todos`)
 console.log(todos)
@@ -25,3 +25,13 @@ todos.shift()
 
 console.log(`You have ${todos.length} todos`)
 console.log(todos)
+
+//challenge 3 
+
+//print all the items:
+    //1. the first item 
+    //2.the second item 
+
+todos.forEach(function(item, index){
+    console.log(`${index + 1}. ${item}`)
+})
